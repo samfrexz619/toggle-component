87678c0fd5a1169aedf073e26fbd0fba705196d7
samfrexz619 <samfrexz69@gmail.com>
2024-03-07 10:06:10 +0100

    add toggle component for light and dark mode that persists across all pages

---
+++ src/component/toggleSwitch/toggleSwitch.tsx
@@ -0,0 +1,28 @@
+import useDark from "../../hooks/useDark";
+import { useState } from "react"
+import './toggle.scss'
+
+const ToggleSwitch = () => {
+
+  const [colorTheme, setTheme] = useDark()
+
+  const [isDark, setIsDark] = useState(colorTheme === 'light' ? true : false)
+
+  const toggleMode = (checked: boolean) => {
+    setTheme(colorTheme)
+    setIsDark(checked)
+  }
+
+  return (
+    <label className='toggle'>
+      <input
+        type="checkbox"
+        checked={isDark}
+        onChange={e => toggleMode(e.target.checked)}
+      />
+      <span className='toggle__slider'></span>
+    </label>
+  );
+}
+
+export default ToggleSwitch;
